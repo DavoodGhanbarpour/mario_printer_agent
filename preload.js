3b7cc14a70ad610f40f18702537c3200ced6b8d4
mahdi2ne1 <mahdimosayyebi80@gmail.com>
2025-05-07 11:14:23 +0330

    feat: some improvments on repo and code

--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    setConfig: (input) => ipcRenderer.send('setConfig', input),
-    getConfig: () => ipcRenderer.send('getConfig'),
-    replyGetConfig: (config) => ipcRenderer.on('replyGetConfig',config),
-    updatePreviewFrame: (callback) => ipcRenderer.on('updatePreviewFrame', callback)
-})
+  setConfig: (input) => ipcRenderer.send('setConfig', input),
+  getConfig: () => ipcRenderer.send('getConfig'),
+  replyGetConfig: (callback) => ipcRenderer.on('replyGetConfig', callback),
+  updatePreviewFrame: (callback) => ipcRenderer.on('updatePreviewFrame', callback),
+});
